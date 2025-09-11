69a72a7d5f91fff46d3aa8c5836763e8062fb2d7
Fortune Mahloane <sensefortune01@gmail.com>
2025-09-11 06:02:06 +0200

    update file references

--- backend/src/routes/activity_routes.js
+++ backend/src/routes/activity_routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const {addActivity, getUserActivities, deleteActivity, getSummary} = require('../controllers/activity_controller');
-const authMiddleware = require('../middleware/authMiddleware');
+const authMiddleware = require('../middleware/auth');
 
 router.post('/', authMiddleware, addActivity);
 router.get('/', authMiddleware, getUserActivities);
