ad671278b114d6d2afdfa95420575f130b418d7f
Fortune Mahloane <sensefortune01@gmail.com>
2025-09-11 09:12:04 +0200

    temp backend url

--- frontend/src/register.js
+++ frontend/src/register.js
@@ -26,7 +26,7 @@ document.addEventListener('DOMContentLoaded',()=>{
         }
 
         try {
-            const response = await fetch('http://localhost:5000/api/register',{
+            const response = await fetch('http://localhost:5000/api/auth/register',{
                 method:"POST",
                 headers:{
                     'Content-Type':'application/json'
