1536378b7560bb717f7191cbb4b7aff5f6f6027e
Fortune Mahloane <sensefortune01@gmail.com>
2025-09-16 18:49:10 +0200

    Add helmet for secure http headers

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,6 +5,7 @@ const connectToDatabase = require("./config/db");
 const errorHandler = require("./middleware/errorHandler");
 const authRoutes = require("./routes/auth_routes");
 const activityRoutes = require("./routes/activity_routes");
+const helmet = require("helmet");
 
 connectToDatabase().catch((err) => {
   console.error("Failed to connect to the database", err);
@@ -14,6 +15,7 @@ connectToDatabase().catch((err) => {
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
